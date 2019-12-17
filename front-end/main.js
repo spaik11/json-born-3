c65b534eb7648585dcbb0bacbd4b1ee33ce8515b
Sung Paik <sung.paik@codeimmersives.com>
2019-12-17 14:33:58 -0500

    included html css

---
+++ front-end/main.js
@@ -0,0 +1,8 @@
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('loadend', (event) => {
+    console.log(event.target.response);
+})
+
+xhr.open('GET', 'http://localhost:3000/users');
+xhr.send();
